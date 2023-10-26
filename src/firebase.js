11f08d9f9812e0f73e62dbd5bd49c940687d8bac
inatoihs <sabanna.taihei@gmail.com>
2023-10-27 00:09:01 +0900

    firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {};// TODO: Add your Firebase project config object here
+
+
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export default firebaseApp;
