1b4656036315d0b215fd1ee72a269c4e5bca29bd
inatoihs <sabanna.taihei@gmail.com>
2023-10-27 09:31:08 +0900

    gitignore

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from 'firebase/app';
-
-const firebaseConfig = {};// TODO: Add your Firebase project config object here
-
-
-
-export const firebaseApp = initializeApp(firebaseConfig);
-export default firebaseApp;
